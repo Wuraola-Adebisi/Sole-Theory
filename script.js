d89d24b435de8bc23afdcbad6d909ffb0adfacf1
Wuraola Adebisi <the.wuraola.adebisi@gmail.com>
2026-04-17 10:33:15 +0100

    Refactor scroll functionality and arrow visibility logic

---
+++ script.js
@@ -0,0 +1,45 @@
+
+const featuredScroll = document.querySelector('.featured-scroll');
+const leftArrow = document.querySelector('.scroll-arrow-left');
+const rightArrow = document.querySelector('.scroll-arrow-right');
+
+function scrollByContainerWidth(direction) {
+  const containerWidth = featuredScroll.offsetWidth;
+
+  const scrollAmount = direction === 'left' ? -containerWidth : containerWidth;
+  
+  featuredScroll.scrollBy({
+    left: scrollAmount,
+    behavior: 'smooth'
+  });
+}
+
+function updateArrowVisibility() {
+  const currentScroll = featuredScroll.scrollLeft;
+  
+  const maxScroll = featuredScroll.scrollWidth - featuredScroll.offsetWidth;
+
+  if (currentScroll === 0) {
+    leftArrow.classList.add('hidden');
+  } else {
+    leftArrow.classList.remove('hidden');
+  }
+  
+  if (Math.ceil(currentScroll) >= maxScroll) {
+    rightArrow.classList.add('hidden');
+  } else {
+    rightArrow.classList.remove('hidden');
+  }
+}
+
+leftArrow.addEventListener('click', () => {
+  scrollByContainerWidth('left');
+});
+
+rightArrow.addEventListener('click', () => {
+  scrollByContainerWidth('right');
+});
+
+featuredScroll.addEventListener('scroll', updateArrowVisibility);
+
+updateArrowVisibility();
